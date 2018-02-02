1313c37e67706e941f921bee8f2ef3c4e404c14f
JimTheMan <mrdotjim@gmail.com>
2018-02-02 00:02:08 -0500

    returns a simple joke object

--- joke-generator/src/joke_generator/core.cljs
+++ joke-generator/src/joke_generator/core.cljs
@@ -1,7 +1,19 @@
 (ns joke-generator.core
   (:require [cljs-lambda.macros :refer-macros [defgateway]]))
 
+(defn getJokeFromLocalArray []
+  {"setup" "A guy walked into a bar...", "punchline" "ouch!"})
+
+
 (defgateway echo [event ctx]
   {:status  200
    :headers {:content-type (-> event :headers :content-type)}
-   :body    (event :body)})
+   :body    (getJokeFromLocalArray)})
+
+
+
+
+;; Array oj Jokes
+[{"setup" "A guy walked into a bar...", "punchline" "ouch!"}
+ {"setup" "2A guy walked into a bar...", "punchline" "3ouch!"}
+ {"setup" "3A guy walked into a bar...", "punchline" "3ouch!"}]
