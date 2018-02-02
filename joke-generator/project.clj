68decf8d2d9e2ad961fec6be0c3f8b6e252f2d16
JimTheMan <mrdotjim@gmail.com>
2018-02-01 23:56:35 -0500

    scaffolded initial project from serverless-cljs

---
+++ joke-generator/project.clj
@@ -0,0 +1,14 @@
+(defproject joke-generator "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure       "1.8.0"]
+                 [org.clojure/clojurescript "1.8.51"]
+                 [io.nervous/cljs-lambda    "0.3.5"]]
+  :plugins [[lein-npm                    "0.6.2"]
+            [io.nervous/lein-cljs-lambda "0.6.6"]]
+  :npm {:dependencies [[serverless-cljs-plugin "0.1.2"]]}
+  :cljs-lambda {:compiler
+                {:inputs  ["src"]
+                 :options {:output-to     "target/joke-generator/joke_generator.js"
+                           :output-dir    "target/joke-generator"
+                           :target        :nodejs
+                           :language-in   :ecmascript5
+                           :optimizations :none}}})
