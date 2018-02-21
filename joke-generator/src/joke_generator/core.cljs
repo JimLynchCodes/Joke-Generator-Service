c8f1ba106549202bcfe81f11c3726d48ded72a7c
JimTheMan <james.lynch@hbo.com>
2018-02-20 23:15:58 -0500

    cleaned up joke-service dangling parens

--- joke-generator/src/joke_generator/core.cljs
+++ joke-generator/src/joke_generator/core.cljs
@@ -1,6 +1,5 @@
 (ns joke-generator.core
-  (:require [cljs-lambda.macros :refer-macros [defgateway]])
-  )
+  (:require [cljs-lambda.macros :refer-macros [defgateway]]))
 
 (def jokes
   [{:setup "A guy walked into a bar..." :punchline "Ouch!" :clean? true}
@@ -8,8 +7,7 @@
    {:setup "Me: Do you think it’s strange to talk to yourself?" :punchLine "Me: No." :clean? true}
    {:setup "What happens to a frog's car when it breaks down?" :punchLine "It gets toad away!" :clean? true}
    {:setup "What do cats eat for breakfast?" :punchLine "Mice krispies!" :clean? true}
-   {:setup "What did the egg say before it got boiled?" :punchLine "It's gonna take me awhile to get hard, I just got laid by a chick!" :clean? false}
-   ])
+   {:setup "What did the egg say before it got boiled?" :punchLine "It's gonna take me awhile to get hard, I just got laid by a chick!" :clean? false}])
 
 (defn getRandomJoke []
   (nth jokes (rand-int (count jokes))))
